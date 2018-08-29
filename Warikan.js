c83d7ba1a8a1070194c8c094d804db32ae446ebf
mayukoro <vc.mii.tantan@gmail.com>
2018-08-30 00:10:41 +0900

    Add input-area

---
+++ Warikan.js
@@ -0,0 +1,15 @@
+(function(){
+    "use strict";
+
+    var price=document.getElementById('price');
+    var num=document.getElementById('num');
+    var unit=document.getElementById('unit');
+    var btn=document.getElementById('btn');
+    var result=document.getElementById('result');
+    var reset=document.getElementById('reset');
+
+    btn.addEventListener('click',function(){
+
+    });
+
+})();
